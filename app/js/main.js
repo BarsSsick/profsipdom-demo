7fb92234de7e7a56e968608ee52853eb2541f639
BarsSsick <95721093+BarsSsick@users.noreply.github.com>
2023-04-17 19:09:53 +0300

    .....

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
-jQuery(document).ready(function () {
+$(document).ready(function () {
+
+
+
+
+
 
   $(document).on("click", ".naccs .menu div", function () {
     var numberIndex = $(this).index();
@@ -17,90 +22,105 @@ jQuery(document).ready(function () {
     }
   });
 
+
+
+
+
   $("#all").click(function () {
     $('input:checkbox').not(this).prop('checked', this.checked);
   });
 
 
-  jqueryDatepicker($);
 
-  $("#datepicker").datepicker({
-    inline: true,
-    changeYear: true,
-    changeMonth: true,
-    mindate: 0,
-    minDate: new Date()
+
+
+
+  // jqueryDatepicker($);
+
+  // $("#datepicker").datepicker({
+  //   inline: true,
+  //   changeYear: true,
+  //   changeMonth: true,
+  //   mindate: 0,
+  //   minDate: new Date()
+  // });
+
+  // jQuery(function ($) {
+  //   $.datepicker.regional['ru'] = {
+  //     closeText: 'Закрыть',
+  //     prevText: '&#x3c;Пред',
+  //     nextText: 'След&#x3e;',
+  //     currentText: 'Сегодня',
+  //     monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+  //       'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+  //     monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+  //       'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+  //     dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+  //     dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+  //     dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+  //     weekHeader: 'Нед',
+  //     dateFormat: 'dd.mm.yy',
+  //     firstDay: 1,
+  //     isRTL: false,
+  //     showMonthAfterYear: false,
+  //     yearSuffix: '',
+  //     mindate: 0,
+  //   };
+  // $.datepicker.setDefaults($.datepicker.regional['ru']);
+// });
+// $(function () {
+//   $('#datepicker').datepicker($.extend({
+//     inline: true,
+//     changeYear: true,
+//     changeMonth: true,
+//     mindate: 0,
+//   },
+//     $.datepicker.regional['ru']
+//   ));
+// });
+
+
+
+
+(function ($) {
+  $('.js-nav-menu-toggle').on('click', function () {
+    $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
   });
 
-  jQuery(function ($) {
-    $.datepicker.regional['ru'] = {
-      closeText: 'Закрыть',
-      prevText: '&#x3c;Пред',
-      nextText: 'След&#x3e;',
-      currentText: 'Сегодня',
-      monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-        'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-        'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-      dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-      dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-      weekHeader: 'Нед',
-      dateFormat: 'dd.mm.yy',
-      firstDay: 1,
-      isRTL: false,
-      showMonthAfterYear: false,
-      yearSuffix: '',
-      mindate: 0,
-    };
-    $.datepicker.setDefaults($.datepicker.regional['ru']);
+  $('html').on('click', function (e) {
+    if (!$(e.target).closest('.js-nav-menu').length &&
+      ($('.js-nav-menu').hasClass('navigation-menu--open'))) {
+      $('.js-nav-menu').removeClass('navigation-menu--open');
+    }
+  });
+})(jQuery);
+
+
+
+
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
   });
 
-  $(function () {
-    $('#datepicker').datepicker($.extend({
-      inline: true,
-      changeYear: true,
-      changeMonth: true,
-      mindate: 0,
-    },
-      $.datepicker.regional['ru']
-    ));
+  $('.scrollup').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 0);
+    return false;
   });
-
-
-
-  (function ($) {
-    $('.js-nav-menu-toggle').on('click', function () {
-      $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
-    });
-
-    $('html').on('click', function (e) {
-      if (!$(e.target).closest('.js-nav-menu').length &&
-        ($('.js-nav-menu').hasClass('navigation-menu--open'))) {
-        $('.js-nav-menu').removeClass('navigation-menu--open');
-      }
-    });
-  })(jQuery);
-
-
-
-
-  $(document).ready(function () {
-
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 500) {
-        $('.scrollup').fadeIn();
-      } else {
-        $('.scrollup').fadeOut();
-      }
-    });
-
-    $('.scrollup').click(function () {
-      $("html, body").animate({ scrollTop: 0 }, 0);
-      return false;
-    });
-
-  });
-  var mixer = mixitup('.project__inner-box');
 
 });
+
+$('.pickadate').pickadate();
+$('.pickatime').pickatime();
+
+
+
+
+// var mixer = mixitup('.project__inner-box');
+
+})
